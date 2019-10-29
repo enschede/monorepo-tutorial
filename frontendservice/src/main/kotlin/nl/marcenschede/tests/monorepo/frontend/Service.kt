f017155d22c16da7d2bacae928615ae28b18b0d0
Marc Enschede <marc@marcenschede.nl>
2019-10-29 19:39:56 +0100

    Cleanup code

--- frontendservice/src/main/kotlin/nl/marcenschede/tests/monorepo/frontend/Service.kt
+++ frontendservice/src/main/kotlin/nl/marcenschede/tests/monorepo/frontend/Service.kt
@@ -2,15 +2,10 @@ package nl.marcenschede.tests.monorepo.frontend
 
 import nl.marcenschede.tests.monorepo.backend.Person
 import nl.marcenschede.tests.monorepo.backend.PersonSearchCriteria
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.reactive.function.BodyExtractors
-import org.springframework.web.reactive.function.BodyInserter
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
